607606774be2b09cdbba4121d239fbff0b948d52
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-04-06 10:12:37 -0400

    Updated mongoose connection string to use IP address

--- models.js
+++ models.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-// const Models = require('./models.js');
 
 // const Movies = Models.Movie;
 // const Users = Models.User;
 
 // mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
