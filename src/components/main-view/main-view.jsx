937f26c79fccd371ee04397342e61039ae453cd8
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-04-18 20:55:42 -0400

    Removed static list and added Fetch call

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,65 +1,46 @@
-import { useState } from 'react';
-import { BookCard } from '../book-card/book-card';
-import { BookView } from '../book-view/book-view';
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 
 export const MainView = () => {
-    const [books, setBooks] = useState([
-        {
-          id: 1,
-          title: "Eloquent JavaScript",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-          author: "Marijn Haverbeke"
-        },
-        {
-          id: 2,
-          title: "Mastering JavaScript Functional Programming",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-          author: "Federico Kereki"
-        },
-        {
-          id: 3,
-          title: "JavaScript: The Good Parts",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-          author: "Douglas Crockford"
-        },
-        {
-          id: 4,
-          title: "JavaScript: The Definitive Guide",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-          author: "David Flanagan"
-        },
-        {
-          id: 5,
-          title: "The Road to React",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-          author: "Robin Wieruch"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
-    const [selectedBook, setSelectedBook] = useState(null);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-    if (selectedBook) {
-        return <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />;
+    useEffect(() => {
+      fetch("https://imdb-api.com/en/API/MostPopularMovies/k_vfofrs3q")
+        .then((response) => response.json())
+        .then((data) => {
+          const moviesFromApi = data.items.map((item) => {
+            return {
+              id: item.id,
+              title: item.title,
+              image: item.image,
+              year: item.year,
+              rank: item.rank
+            };
+          });
+          setMovies(moviesFromApi);
+        });
+    }, []);
+
+    if (selectedMovie) {
+        return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
     }
 
-    if (books.length === 0) {
+    if (movies.length === 0) {
         return <div>The list is empty!</div>
     }
 
     return (
       <div>
-        {books.map((book) => (
-          <BookCard 
-            key={book.id} 
-            book={book} 
-            onBookClick={(newSelectedBook) => {
-              setSelectedBook(newSelectedBook);
+        {movies.map((movie) => (
+          <MovieCard 
+            key={movie.id} 
+            movie={movie} 
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
             }}
           />
         ))}
