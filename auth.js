ab4b99b17c8b69d3299bee252baf443e98d8759f
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-04-20 12:59:03 -0400

    Trying to get back to where I started

--- auth.js
+++ auth.js
@@ -22,7 +22,6 @@ module.exports = (router) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
 //            console.log(error, info);
 //            console.log(user);
-//            console.log(req);
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
