6976cb8246c3c8f925024d254342a15b952e40ac
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-04-24 16:30:07 -0400

    Added code for React Container

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
 //import React from 'react';
 
 // Import statement to indicate that you need to bundle `./index.scss`
@@ -7,7 +8,11 @@ import "./index.scss";
 
 // Main component (will eventually use all the others)
 const App = () => {
-    return <MainView />;
+    return (
+        <Container>
+            <MainView />
+        </Container>
+    );
 };
 
 // Finds the root of your app
