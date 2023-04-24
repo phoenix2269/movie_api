0e008ec5a6e9f9c99dd553b7d784aa805471cc9f
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-04-24 16:31:30 -0400

    updated code for react router

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,33 @@
-export const MovieView = ({ movie, onBackClick }) => {
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import './movie-view.scss';
+
+export const MovieView = ({ movie }) => {
+    const { movieId } = useParams();
+    const movie = movies.find((b) => b.id === movieId);
+
     return (
         <div>
+            <div>
+                <img className="w-100" src={movie.image} />
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Release Year: </span>
+                <span>{movie.year}</span>
+            </div>
+            <div>
+                <span>Rating: </span>
+                <span>{movie.rating}</span>
+            </div>
+            <Link to={"/"}>
+                <button className="back=button">Back</button>
+            </Link>
+        </div>
+/*         <div>
             <div>
                 <img src={movie.image} />
             </div>
@@ -17,6 +44,6 @@ export const MovieView = ({ movie, onBackClick }) => {
                 <span>{movie.rating}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
-        </div>
+        </div> */
     );
 };
