c7e87b58c2a41ceb929e34cfe51e2728dc4bb0a0
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-06-21 14:59:27 -0400

    Updated code to use react-bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import './movie-view.scss';
 
-export const MovieView = ({ movie }) => {
+export const MovieView = ({ movies }) => {
     const { movieId } = useParams();
     const movie = movies.find((b) => b.id === movieId);
 
@@ -24,26 +25,8 @@ export const MovieView = ({ movie }) => {
                 <span>{movie.rating}</span>
             </div>
             <Link to={"/"}>
-                <button className="back=button">Back</button>
+                <Button className="back=button">Back</Button>
             </Link>
         </div>
-/*         <div>
-            <div>
-                <img src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Release Year: </span>
-                <span>{movie.year}</span>
-            </div>
-            <div>
-                <span>Rating: </span>
-                <span>{movie.rating}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div> */
     );
 };
