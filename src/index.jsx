b865fb4c67f7b16f45d6298211d29ab42188ba8b
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-06-21 20:12:21 -0400

    Changed const tp App and removed border

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,23 @@
-import { createRoot } from "react-dom/client";
-import { MainView } from "./components/main-view/main-view";
-import Container from "react-bootstrap/Container";
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
+//import React from 'react';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-//Main component (will eventually use all the others)
-const MyFlixApplication = () => {
+// Main component (will eventually use all the others)
+const App = () => {
     return (
-        <Container style={{border: "1px solid red"}}>
+        <Container>
             <MainView />
         </Container>
     );
 };
 
-//Finds the root of your app
+// Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-//Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+// Tells React to render your app in the rooot DOM element
+root.render(<App />);
