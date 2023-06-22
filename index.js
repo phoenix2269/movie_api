cbaeebf1496119089f68012a42cd05aa4984c733
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-06-21 21:38:17 -0400

    Just trying to  sync

--- index.js
+++ index.js
@@ -60,8 +60,7 @@ app.get('/', (req, res) => {
 })
 
 // GET list of ALL movies
-// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
